68a61c97d790732b806f4381f64922a89354224b
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-04-19 16:21:34 +0200

    Check button works. Message gets updated. Calculation of the Score works.

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 'use strict';
+// define secret number once we start application! to the secret number we will compare all the guesses. secretNumber - is state variable.
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// variable score keeps data in our code! which is good! we don't need to rely on the DOM to keep our data. it is STATE VARIABLE- because it is relavant to the application.
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
-console.log(document.querySelector('.message').textContent);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ Provide a number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🌟 Correct Number!';
+    score++;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⛔️ Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥  You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⛔️ Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});

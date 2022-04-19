cc4372138976b25b1956ada8f80eb6cbed360427
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-04-19 20:28:25 +0200

    Game reset functionality

--- script.js
+++ script.js
@@ -3,17 +3,23 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 // variable score keeps data in our code! which is good! we don't need to rely on the DOM to keep our data. it is STATE VARIABLE- because it is relavant to the application.
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  //no number provided
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ Provide a number';
+
+    // player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🌟 Correct Number!';
+    document.querySelector('.message').textContent = '🌟 You won!';
     score++;
     document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '⛔️ Too high!';
@@ -33,4 +39,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
 });
